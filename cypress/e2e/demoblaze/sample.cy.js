7942721da8e72fb2804af088e6f47235a5660d31
uki0604 <118947049+uki0604@users.noreply.github.com>
2023-03-10 08:55:28 +0700

    Update sample.cy.js

--- cypress/e2e/demoblaze/sample.cy.js
+++ cypress/e2e/demoblaze/sample.cy.js
@@ -1,8 +1,1 @@
-describe('login tokoilmu scenario', () => {
-    it('successfully login using correct username password', () => {
-        cy.visit('https://tokoilmu.id/login')
-        cy.get("//*[@id="__next"]/div/div/div/div[2]/div/form/div[2]/input").type('marzuki@prospark.co').should('have.value', 'marzuki@prospark.co')
-        cy.get("//*[@id="__next"]/div/div/div/div[2]/div/form/div[3]/input").type('123456').should('have.value', '123456')
-        cy.get("//button[@class='text-white text-md w-100 btn btn-primary']").click()
-    })
-})
+
